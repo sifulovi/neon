89945a765537f3985cb6ec8cca083a97a89f8016
ove <isiful.ovi@gmail.com>
2023-09-26 22:52:07 +0600

    Bismillah

---
+++ src/main/java/com/sio/neon/NeonApplication.java
@@ -0,0 +1,13 @@
+package com.sio.neon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sio/neon/NeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sio.neon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
